1b6b5c14dab32f9a3ad9164840c80f568804e1a3
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-14 01:39:41 +0000

    task 1 setup

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list- function that sorts a doubly 
+ * linked list of integers in ascending order
+ * @list: list 
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	
+}
