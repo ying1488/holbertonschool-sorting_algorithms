0ecf3ab3b04a4c565fd2099fa1235bd4c0281e16
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-14 02:33:12 +0000

    fixed betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,7 +5,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list- function that sorts a doubly 
+ * insertion_sort_list- function that sorts a doubly
  * linked list of integers in ascending order
  * @list: listint_t
  */
@@ -18,26 +18,25 @@ void insertion_sort_list(listint_t **list)
 		return;
 
 	current = (*list)->next;
-	
+
 	while (current)
 	{
 		temp = current;
-
 		while (temp->prev && temp->n < temp->prev->n)
-			{
-				a = temp ->prev;
-				b = temp;
-			       	a->next = b->next;
-				if(b->next)
-					b->next->prev = a;
+		{
+			a = temp->prev;
+			b = temp;
+			a->next = b->next;
+			if (b->next)
+				b->next->prev = a;
 				b->prev = a->prev;
 				b->next = a;
-				if(a->prev)
+				if (a->prev)
 					a->prev->next = b;
 				else
 					*list = b;
 				a->prev = b;
-			print_list(*list);	
+				print_list(*list);
 			}
 		current  = current->next;
 	}
