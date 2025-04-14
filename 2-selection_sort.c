f6b21766727d733edc192f830c6bb2c2bb9acfb9
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-14 04:01:04 +0000

    betty approved

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,26 +16,26 @@ void selection_sort(int *array, size_t size)
 	size_t currE, comI, minI;
 	int temp;
 
-	if(array == NULL || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (currE = 0; currE < size -1; currE++)
+	for (currE = 0; currE < size - 1; currE++)
 	{
-		// Assume the current position holds
-        	// the minimum element
+		/*Assume the current position holds*/
+		/*the minimum element*/
 		minI = currE;
-		// Iterate through the unsorted portion
-        	// to find the actual minimum
+		/* Iterate through the unsorted portion*/
+		/* to find the actual minimum*/
 		for (comI = currE + 1; comI < size; comI++)
 		{
 			if (array[comI] < array[minI])
 			{
-				// Update min_idx if a smaller element is found
+				/*Update min_idx if a smaller element is found*/
 				minI = comI;
 			}
 		}
-		// Move minimum element to its
-		// // correct position
+		/* Move minimum element to its*/
+		/*correct position*/
 		if (minI != currE)
 		{
 			temp = array[currE];
